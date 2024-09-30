38a33bebe1e429ea80e88ba8ed19efde6ea6b7e3
JanŠtefin <jan.stefin@sonce.com>
2024-09-30 11:16:45 +0200

    fixed UI

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,8 +1,8 @@
-var = {
+{
     "name": "Estate",  # The name that will appear in the App list
     "version": "16.0.0",  # Version
     "application": True,
-    # This line says the module is an App, and not a module1
+    # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": ["security/ir.model.access.csv",
              "views/estate_property_views.xml"],
