8c2ddd285b1fec22733b66d47fd01a7f297bad77
JanŠtefin <jan.stefin@sonce.com>
2024-09-30 16:07:16 +0200

    fixed manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,8 @@
     "data": ["security/ir.model.access.csv",
              "views/estate_property_views.xml",
             "views/offer_views.xml",
+"views/tags_views.xml",
+"views/types_views.xml",
              "views/estate_menus.xml",
              ],
     "installable": True,
