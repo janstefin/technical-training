db20e96369280e21fe0cfff24ca398fcc8d05eee
JanŠtefin <jan.stefin@sonce.com>
2024-09-30 14:40:54 +0200

    fixed seperator

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,11 +5,11 @@ class EstateProperty(models.Model):
     _description = "Estate Property model"
 
     name = fields.Char(required=True, default="Unknown")
-    state = fields.Selection([("new", "New"), ("offer_received", "Offer Received")])
+    state = fields.Selection([("new", "New"), ("offer_received", "Offer Received"), ("sold", "Sold")])
     description = fields.Text()
     postcode = fields.Integer()
     date = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(readonly=True, copy=True)
     garden = fields.Boolean()
     bedrooms = fields.Integer()
     garden_orientation = fields.Selection([("south", "South"), ("north", "North")])
