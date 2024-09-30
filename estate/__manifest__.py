392ca58f585990e6e43ee576969ac944c9ee37c9
JanŠtefin <jan.stefin@sonce.com>
2024-09-30 11:51:41 +0200

    added UI menuitem

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -4,7 +4,8 @@
     "application": True,
     # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
-    "data": ["views/estate_property_views.xml"],
+    "data": ["views/estate_property_views.xml",
+             "views/estate_menus.xml"],
     "installable": True,
     'license': 'LGPL-3',
 }
