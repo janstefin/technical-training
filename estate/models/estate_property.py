5f9f36150bd2ff6e5ce25d36ba1455d64159f185
JanŠtefin <jan.stefin@sonce.com>
2024-09-30 16:31:14 +0200

    fixed permissions

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -14,6 +14,9 @@ class EstateProperty(models.Model):
     bedrooms = fields.Integer()
     garden_orientation = fields.Selection([("south", "South"), ("north", "North")])
     last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
+
+    property_type_id = fields.Many2one("estate_property.types")
+    tag_ids = fields.Many2many("estate_property.tags")
     salesperson_id = fields.Many2one("res.users", default = lambda self: self.env.user)
     buyer_id = fields.Many2one("res.partner", copy=False)
     offer_ids = fields.One2many("estate_property.offer", "property_id")
