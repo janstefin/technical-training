02c6b6c928717e14ad22c6a3321b2c859f4233c3
JanŠtefin <jan.stefin@sonce.com>
2024-09-30 15:30:24 +0200

    fixed manifest

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,8 +6,9 @@
     "depends": ["base"],  # dependencies
     "data": ["security/ir.model.access.csv",
              "views/estate_property_views.xml",
+            "views/offer_views.xml",
              "views/estate_menus.xml",
-             "views/offer_views.xml"],
+             ],
     "installable": True,
     'license': 'LGPL-3',
 }
