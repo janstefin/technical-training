577dbf029030d7fb30c85aa4c6716cf11b7834ac
JanŠtefin <jan.stefin@sonce.com>
2024-09-30 11:04:31 +0200

    added secuirty and UI

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,11 +1,11 @@
-{
+var = {
     "name": "Estate",  # The name that will appear in the App list
     "version": "16.0.0",  # Version
-    "application": True,  # This line says the module is an App, and not a module
+    "application": True,
+    # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
-    "data": [
-
-    ],
+    "data": ["security/ir.model.access.csv",
+             "views/estate_property_views.xml"],
     "installable": True,
     'license': 'LGPL-3',
 }
