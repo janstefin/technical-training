3ad3a1b2067717b5662482c1d4ba8f3604da9eb4
JanŠtefin <jan.stefin@sonce.com>
2024-09-30 11:40:05 +0200

    fixed secuirty

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,10 +1,9 @@
 from odoo import fields, models
 
 class EstateProperty(models.Model):
-    _name = "estate.property"
+    _name = "estate_property"
     _description = "Estate Property model"
 
     name = fields.Char(required=True)
     description = fields.Text()
     date = fields.Date()
-    garden_orientation = fields.Selection([("north", "North"), ("south, South"), ("east", "East")])
