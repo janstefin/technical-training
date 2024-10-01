a49ef1d43ebbf26560f58e8c0c454ea87c02e191
JanŠtefin <jan.stefin@sonce.com>
2024-10-01 14:19:57 +0200

    added invocing

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,29 +7,7 @@ class EstateProperty(models.Model):
     _name = "estate_property"
     _description = "Estate Property model"
 
-    def action_sell_property(self):
-        vals_list=[]
-        for property in self:
-            vals = {
-                "partner_id": property.buyer_id.id,
-                "move_type": "out_invoice",
-                "journal_id": 1,
-                "invoice_line_ids":[
-                    Command.create({
-                        "name": property.name,
-                        "quantity": 1,
-                        "price_unit": 0.06*property.selling_price
-                    }),
-                    Command.create({
-                        "name": "Administration Fees",
-                        "quantity": 1,
-                        "price_unit": 100
-                    })
-                ]
-            }
-            vals_list.append(vals)
-        self.env["account.move"].create(vals_list)
-        return super().action_sell_property()
+
 
     name = fields.Char(required=True)
 
@@ -110,12 +88,32 @@ class EstateProperty(models.Model):
 
 
 
-
     def action_sell_property(self):
+        vals_list=[]
         for property in self:
             if property.state == "canceled":
                 raise UserError(_("Canceled properties cannot be sold"))
             property.state = "sold"
+            vals = {
+                "partner_id": property.buyer_id.id,
+                "move_type": "out_invoice",
+                "journal_id": 1,
+                "invoice_line_ids":[
+                    Command.create({
+                        "name": property.name,
+                        "quantity": 1,
+                        "price_unit": 0.06*property.selling_price
+                    }),
+                    Command.create({
+                        "name": "Administration Fees",
+                        "quantity": 1,
+                        "price_unit": 100
+                    })
+                ]
+            }
+            vals_list.append(vals)
+        self.env["account.move"].create(vals_list)
+        return super().action_sell_property()
 
     def action_cancel_property(self):
         self.state = "canceled"
