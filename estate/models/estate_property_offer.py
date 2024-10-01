4b417cef90dab557971d2fda000790634b53b32e
JanŠtefin <jan.stefin@sonce.com>
2024-10-01 10:46:56 +0200

    added tree view in offers

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,23 +1,36 @@
 from odoo import fields, models, api
 
+
 class EstatePropertyOffer(models.Model):
     _name = "estate_property.offer"
     _description = "Estate Property Offer"
 
     price = fields.Float()
-    status = fields.Selection([("accepted", "Accepted"), ("refused", "Refused")])
+    status = fields.Selection(
+        [("accepted", "Accepted"), ("refused", "Refused")])
     property_id = fields.Many2one("estate_property", required=True)
     partner_id = fields.Many2one("res.partner", required=True)
     validity = fields.Integer(default=7)
-    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+    date_deadline = fields.Date(compute="_compute_date_deadline",
+                                inverse="_inverse_date_deadline")
 
     @api.depends("validity", "create_date")
     def _compute_date_deadline(self):
         for estate in self:
             create_date = estate.create_date or fields.Date.today()
-            estate.date_deadline = fields.Date.add(create_date, days=estate.validity)
+            estate.date_deadline = fields.Date.add(create_date,
+                                                   days=estate.validity)
 
     def _inverse_date_deadline(self):
         for estate in self:
             create_date = estate.create_date or fields.Date.today()
-            estate.validity = (estate.date_deadline - fields.Date.to_date(create_date)).days
+            estate.validity = (estate.date_deadline - fields.Date.to_date(
+                create_date)).days
+
+    def action_accept_offer(self):
+        self.status = "accepted"
+        for offer in self:
+            self.property_id.selling_price = self.price
+
+    def action_refuse_offer(self):
+        self.status = "refused"
