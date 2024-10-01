bf8f9334531b824e13e129e52d55e1fb6455a15d
JanŠtefin <jan.stefin@sonce.com>
2024-10-01 09:02:02 +0200

    fixed validaty to Intiger

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -8,5 +8,5 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection([("accepted", "Accepted"), ("refused", "Refused")])
     property_id = fields.Many2one("estate_property", required=True)
     partner_id = fields.Many2one("res.partner", required=True)
-    validity = fields.Number()
+    validity = fields.Integer()
     deadline = fields.Date(default=fields.Datetime.now, days=14)
