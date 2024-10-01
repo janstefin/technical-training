7fd040aade690e81b37c21fe53f30b4d43b3deac
JanŠtefin <jan.stefin@sonce.com>
2024-10-01 09:23:16 +0200

    aded deadline compute

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstatePropertyOffer(models.Model):
     _name = "estate_property.offer"
@@ -8,5 +8,12 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection([("accepted", "Accepted"), ("refused", "Refused")])
     property_id = fields.Many2one("estate_property", required=True)
     partner_id = fields.Many2one("res.partner", required=True)
-    validity = fields.Integer()
-    deadline = fields.Date(default=fields.Datetime.now, days=14)
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline")
+    create_date = fields.Date(readonly=True, default=fields.Datetime.now)
+
+    @api.depends("validity", "create_date")
+    def _compute_date_deadline(self):
+        for estate in self:
+            create_date = estate.create_date or fields.Date.today()
+            estate.date_deadline = fields.Date.add(create_date, days = estate.validity)
