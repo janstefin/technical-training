4b902aa18194e82143f54d7202c1c3ba29335340
JanŠtefin <jan.stefin@sonce.com>
2024-10-01 13:45:24 +0200

    fixed init

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    property_ids = fields.One2many("estate_property" , "sales_person_id", domain=[("state", "not in", ["sold", "canceled"])])
+    property_ids = fields.One2many("estate_property", "salesperson_id", domain=[("state", "not in", ["sold", "canceled"])])
